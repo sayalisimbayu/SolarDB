8863d1a79089e3a82dac2987750ee37a008c79b4
Amar Barge <Amar.Barge@gep.com>
2020-09-14 17:39:59 +0530

    Theme changes

--- dbo/Tables/APPUSERADDONCONFIG.sql
+++ dbo/Tables/APPUSERADDONCONFIG.sql
@@ -1,11 +1,15 @@
 ﻿CREATE TABLE [dbo].[APPUSERADDONCONFIG] (
     [ID]                INT            IDENTITY (1, 1) NOT NULL,
     [USID]              INT            NOT NULL,
-    [LANGUAGE]          VARCHAR (200)  NOT NULL,
-    [TIMEZONE]          VARCHAR (200)  NOT NULL,
-    [DATEFORMAT]        VARCHAR (200)  NOT NULL,
-    [LUCIDNOTIFICATION] NVARCHAR (250) NOT NULL,
+    [LANGUAGE]          VARCHAR (200)  DEFAULT ('') NOT NULL,
+    [TIMEZONE]          VARCHAR (200)  DEFAULT ('') NOT NULL,
+    [DATEFORMAT]        VARCHAR (200)  DEFAULT ('') NOT NULL,
+    [LUCIDNOTIFICATION] NVARCHAR (250) DEFAULT ('') NOT NULL,
+    [THEME]             NVARCHAR (10)  DEFAULT ('') NOT NULL,
+    [SKIN]              NVARCHAR (10)  DEFAULT ('theme-blue') NOT NULL,
     PRIMARY KEY CLUSTERED ([ID] ASC),
     FOREIGN KEY ([USID]) REFERENCES [dbo].[APPUSER] ([ID])
 );
+
+
 
